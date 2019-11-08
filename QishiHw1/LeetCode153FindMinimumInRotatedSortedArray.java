c2ffab36b5415aef97779cae99befa47a251acb2
TaylorQiu <57326818+TaylorXQiu@users.noreply.github.com>
2019-11-08 16:40:33 -0600

    Update LeetCode153FindMinimumInRotatedSortedArray.java

--- QishiHw1/LeetCode153FindMinimumInRotatedSortedArray.java
+++ QishiHw1/LeetCode153FindMinimumInRotatedSortedArray.java
@@ -2,23 +2,25 @@
 
 class Solution {
     public int findMin(int[] nums) {
-        if(nums == null || nums.length == 0) return -1;
-        if(nums.length == 1 || nums[0] < nums[nums.length - 1]) return nums[0];
+        if (nums == null || nums.length == 0)
+        	return -1;
 
-        int barrier = nums[nums.length - 1];
-        int start = 0, end = nums.length - 1;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+        	if (nums[left] < nums[right]) {
+        		break;
+        	}
 
-        while(start + 1 < end) {
-            int mid = (start + end) / 2;
+        	int mid = left + (right - left) / 2;
 
-            if(nums[mid] > barrier) start = mid;
-            else if(nums[mid] < barrier) end = mid;
+        	if (nums[mid] >= nums[left]) {
+        		left = mid + 1;
+        	} else {
+        		right = mid;
+        	}
+
         }
 
-        if(nums[start] <= barrier) return nums[start];
-        if(nums[end] <= barrier) return nums[end];
-
-        return Integer.MAX_VALUE;
+        return nums[left];
     }
-       
 }
